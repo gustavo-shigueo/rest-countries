ce2bb312fe38dfeb3f983e24e6358d007c485136
gustavo-shigueo <gustavo.gsmn@gmail.com>
2024-02-25 19:20:55 -0300

    Minor fixes

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,37 +1,37 @@
-import { formatNumber } from "src/formatters/number";
-import styles from "./Card.module.css";
-import { formatList } from "src/formatters/list";
-import { A } from "@solidjs/router";
+import { formatNumber } from 'src/formatters/number'
+import styles from './Card.module.css'
+import { formatList } from 'src/formatters/list'
+import { A } from '@solidjs/router'
 
 type CardProps = {
-  flag: { src: string; alt: string };
-  name: string;
-  population: number;
-  capital: string[];
-  region: string;
-};
+	flag: { src: string; alt: string }
+	name: string
+	population: number
+	capital: string[]
+	region: string
+}
 
 export function Card(props: CardProps) {
-  return (
-    <article class={styles.card + " card"}>
-      <A href={`/details/${props.name}`}>
-        <div class={styles.flag}>
-          <img src={props.flag.src} alt={props.flag.alt} />
-        </div>
+	return (
+		<article class={styles.card}>
+			<A href={`/details/${props.name}`}>
+				<div class={styles.flag}>
+					<img src={props.flag.src} alt={props.flag.alt} />
+				</div>
 
-        <section class={styles.info}>
-          <h2>{props.name}</h2>
-          <p>
-            <strong>Population: </strong> {formatNumber(props.population)}
-          </p>
-          <p>
-            <strong>Region: </strong> {props.region}
-          </p>
-          <p>
-            <strong>Capital: </strong> {formatList(props.capital)}
-          </p>
-        </section>
-      </A>
-    </article>
-  );
+				<section class={styles.info}>
+					<h2>{props.name}</h2>
+					<p>
+						<strong>Population: </strong> {formatNumber(props.population)}
+					</p>
+					<p>
+						<strong>Region: </strong> {props.region}
+					</p>
+					<p>
+						<strong>Capital: </strong> {formatList(props.capital)}
+					</p>
+				</section>
+			</A>
+		</article>
+	)
 }
--- src/pages/Index/index.tsx
+++ src/pages/Index/index.tsx
@@ -1,80 +1,84 @@
-import "./index.css";
-import { Header } from "src/components/Header";
-import { For, Suspense, createResource, createSignal } from "solid-js";
-import { CountryOverview } from "src/types/CountryOverview";
-import { Card } from "src/components/Card";
-import { Input } from "src/components/Input";
-import { IoSearch } from "solid-icons/io";
-import { createDebounce } from "src/signals/createDebounce";
-import { Select } from "src/components/Select";
-import { Loading } from "src/components/Loading";
+import './index.css'
+import { Header } from 'src/components/Header'
+import { For, Suspense, createResource, createSignal } from 'solid-js'
+import { CountryOverview } from 'src/types/CountryOverview'
+import { Card } from 'src/components/Card'
+import { Input } from 'src/components/Input'
+import { IoSearch } from 'solid-icons/io'
+import { createDebounce } from 'src/signals/createDebounce'
+import { Select } from 'src/components/Select'
+import { Loading } from 'src/components/Loading'
 
-const url = new URL("all", import.meta.env.VITE_API_URL);
-url.searchParams.set("fields", "name,capital,region,population,flags,cca3");
+const url = new URL('all', import.meta.env.VITE_API_URL)
+url.searchParams.set('fields', 'name,capital,region,population,flags,cca3')
 
 function Index() {
-  const [search, setSearch] = createDebounce("");
-  const [region, setRegion] = createSignal("");
-  const [countries] = createResource(async (): Promise<CountryOverview[]> => {
-    const data: CountryOverview[] = await fetch(url).then((x) => x.json());
-    return data.sort((a, b) => a.name.common.localeCompare(b.name.common));
-  });
+	const [search, setSearch] = createDebounce('')
+	const [region, setRegion] = createSignal('')
+	const [countries] = createResource(async (): Promise<CountryOverview[]> => {
+		const data: CountryOverview[] = await fetch(url).then(x => x.json())
+		return data.sort((a, b) => a.name.common.localeCompare(b.name.common))
+	})
 
-  return (
-    <>
-      <Header />
-      <Suspense fallback={<Loading />}>
-        <main>
-          <div class="search">
-            <Input
-              placeholder="Search for a country..."
-              type="search"
-              icon={<IoSearch />}
-              value={search()}
-              onInput={(e) => setSearch(e.currentTarget.value)}
-            />
+	return (
+		<>
+			<Header />
+			<Suspense fallback={<Loading />}>
+				<main>
+					<div class="search">
+						<Input
+							placeholder="Search for a country..."
+							type="search"
+							icon={<IoSearch />}
+							value={search()}
+							onInput={e => setSearch(e.currentTarget.value)}
+						/>
 
-            <Select onChange={(e) => setRegion(e.currentTarget.value)}>
-              <option value="">Filter by Region</option>
-              <For
-                each={[
-                  ...new Set(countries()?.map((x) => x.region) ?? []),
-                ].sort()}
-              >
-                {(x) => <option>{x}</option>}
-              </For>
-            </Select>
-          </div>
+						<Select
+							name="Region"
+							aria-label="Filter by region"
+							onChange={e => setRegion(e.currentTarget.value)}
+						>
+							<option value="">Filter by Region</option>
+							<For
+								each={[
+									...new Set(countries()?.map(x => x.region) ?? []),
+								].sort()}
+							>
+								{x => <option>{x}</option>}
+							</For>
+						</Select>
+					</div>
 
-          <div class="countries">
-            <For
-              each={countries()?.filter((x) => {
-                const regionMatch = region() === "" || x.region === region();
+					<div class="countries">
+						<For
+							each={countries()?.filter(x => {
+								const regionMatch = region() === '' || x.region === region()
 
-                if (!regionMatch) {
-                  return false;
-                }
+								if (!regionMatch) {
+									return false
+								}
 
-                return x.name.common
-                  .toLocaleLowerCase()
-                  .includes(search().toLocaleLowerCase());
-              })}
-            >
-              {(country) => (
-                <Card
-                  name={country.name.common}
-                  population={country.population}
-                  capital={country.capital}
-                  region={country.region}
-                  flag={{ src: country.flags.png, alt: country.flags.alt }}
-                />
-              )}
-            </For>
-          </div>
-        </main>
-      </Suspense>
-    </>
-  );
+								return x.name.common
+									.toLocaleLowerCase()
+									.includes(search().toLocaleLowerCase())
+							})}
+						>
+							{country => (
+								<Card
+									name={country.name.common}
+									population={country.population}
+									capital={country.capital}
+									region={country.region}
+									flag={{ src: country.flags.png, alt: country.flags.alt }}
+								/>
+							)}
+						</For>
+					</div>
+				</main>
+			</Suspense>
+		</>
+	)
 }
 
-export default Index;
+export default Index
